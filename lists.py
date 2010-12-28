482c5fe6d1bb130fd82318a07e839f7af419261c
wagonlips <sean@seancamden.com>
2010-12-28 14:21:51 -0800

    More list fun.

--- lists.py
+++ lists.py
@@ -2,5 +2,25 @@
 import sys
 def main():
 
+  fruit = ['oranges', 'bananas', 'figs', 'hot house grapes']
+  for i in fruit:
+    print i
+
+  print " "
+
+  fruit.append('apples')
+  for i in fruit:
+    print i
+
+  print " "
+
+  fruity = []
+  fruity.append('insectosaurus')
+  fruity.append('frogosaur')
+  fruity.append('penguidactyl')
+  print fruity[2]
+  for i in fruity:
+    print i
+
 if __name__ == '__main__':
   main()
